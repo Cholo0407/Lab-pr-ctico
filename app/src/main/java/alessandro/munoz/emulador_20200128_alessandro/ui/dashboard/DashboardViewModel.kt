cd7c2398dfb1c8c3509b4762fe86db7f69a5813e
Ale <aless070704@gmail.com>
2024-05-03 19:20:37 -0600

    cree el repositorio

---
+++ app/src/main/java/alessandro/munoz/emulador_20200128_alessandro/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package alessandro.munoz.emulador_20200128_alessandro.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/alessandro/munoz/emulador_20200128_alessandro/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package alessandro.munoz.emulador_20200128_alessandro.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
